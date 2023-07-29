6e640d7483b73869c757dedb4d94deb569be9506
gouthamraj020102 <gouthamraj020102@gmail.com>
2023-07-29 11:44:28 +0530

    Addded lombok for Entity

--- src/main/java/com/sampleProject/springboot/Entities/Student.java
+++ src/main/java/com/sampleProject/springboot/Entities/Student.java
@@ -1,57 +1,17 @@
 package com.sampleProject.springboot.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
-
-    public Student() {
-    }
-
-    public Student(int id, String name, String nickName, String description) {
-        this.id = id;
-        this.name = name;
-        this.nickName = nickName;
-        this.description = description;
-    }
 
     public int id;
     public String name;
     public String nickName;
     public String description;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Student [id=" + id + ", name=" + name + ", nickName=" + nickName + ", description=" + description + "]";
-    }
 
 }

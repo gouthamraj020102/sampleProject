d1f789c4b270982287f35747b96a554bb5b7adc9
gouthamraj020102 <gouthamraj020102@gmail.com>
2023-07-30 16:01:32 +0530

    MySQL connection

---
+++ src/main/java/com/sampleProject/springboot/Entities/Employee.java
@@ -0,0 +1,24 @@
+package com.sampleProject.springboot.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private double experience;
+    private String role;
+}
